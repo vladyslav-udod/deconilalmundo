91abe656eb2f9c5a8ee17e86a644149aaf3dd76d
Vladyslav Udod <vladyslav.udod@cellebrite.com>
2026-06-03 11:27:47 +0200

    basePath sanity added

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
-import { esESLocale } from '@sanity/locale-es-es'
-import { schemaTypes } from './sanity/schemaTypes'
-import { structure } from './sanity/structure'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { esESLocale } from "@sanity/locale-es-es";
+import { schemaTypes } from "./sanity/schemaTypes";
+import { structure } from "./sanity/structure";
 
 export default defineConfig({
-  name: 'de-conil-al-mundo',
-  title: 'De Conil al Mundo',
+  name: "de-conil-al-mundo",
+  title: "De Conil al Mundo",
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+  basePath: "/studio",
 
   plugins: [
     // Interfaz del Studio en español (menús, calendario, botones…).
@@ -22,4 +23,4 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+});
